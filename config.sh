5f21e5dc985dd77fa4376985a26a76e2ecdec4ce
wu <hsnuhayato@gmail.com>
2019-05-22 13:50:24 +0900

    update config.sh

--- config.sh
+++ config.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 export COMP_NAME="wuTest"
+export PYTHONPATH=$PYTHONPATH:/usr/lib/omniidl
 export PATH=/usr/pkg/bin:$PATH
 export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig:/usr/local/lib64/pkgconfig
-export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib/openrtm-1.1:/usr/local/lib64:/usr/lib64/openrtm-1.1
+export LD_LIBRARY_PATH=/usr/local/lib:/usr/lib/x86_64-linux-gnu/openrtm-1.1:/usr/lib/openrtm-1.1:/usr/local/lib64:/usr/lib64/openrtm-1.1
